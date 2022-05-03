d6a68120903cdd83a4831b8ecfae14b715ba21aa
Ervin <evgnz24@gmail.com>
2022-05-02 23:08:00 -0500

    Added QuerySelectors for all the elements in the calculator, Also added a Calculator class with a constructor and some functions

--- script.js
+++ script.js
@@ -1,93 +1,30 @@
-const calculatorEl = document.querySelector('.calculator');
-const displayEl = document.querySelector('.display');
-const calcNode = document.querySelectorAll('.calculator');
+'use strict';
+
 const clearEl = document.querySelector('.clear');
 const deleteEl = document.querySelector('.delete');
-const percentEl = document.querySelector('.percent');
-const decimalEl = document.querySelector('.decimal');
-const divideEl = document.querySelector('.divide');
-const multiplyEl = document.querySelector('.multiply');
-const subtractEl = document.querySelector('.subtract');
-const additionEl = document.querySelector('.addition');
-const equalsEl = document.querySelector('.equals');
-const zeroEl = document.getElementById('zero');
-const oneEl = document.getElementById('one');
-const twoEl = document.getElementById('two');
-const threeEl = document.getElementById('three');
-const fourEl = document.getElementById('four');
-const fiveEl = document.getElementById('five');
-const sixEl = document.getElementById('six');
-const sevenEl = document.getElementById('seven');
-const eightEl = document.getElementById('eight');
-const nineEl = document.getElementById('nine');
+const equalEl = document.querySelector('.equal');
+const numberElNode = document.querySelectorAll('[data-number]');
+const operatorElNode = document.querySelectorAll('[data-operator]');
+const previousNumEl = document.querySelector('.previous-number');
+const currentNumEl = document.querySelector('.current-number');
 
-const btnObj = [
-  clearEl,
-  deleteEl,
-  percentEl,
-  decimalEl,
-  divideEl,
-  multiplyEl,
-  subtractEl,
-  additionEl,
-  equalsEl,
-  zeroEl,
-  oneEl,
-  twoEl,
-  threeEl,
-  fourEl,
-  fiveEl,
-  sixEl,
-  sevenEl,
-  eightEl,
-  nineEl,
-];
-
-let memory = [];
-
-btnObj.forEach((btn) =>
-  btn.addEventListener('click', function (e) {
-    if (!isNaN(e.target.textContent)) {
-      console.log(e.key);
-      memory.push(+e.target.textContent);
-      console.log(memory.join(''));
-      return (displayEl.textContent = +e.target.textContent);
-    }
-    if (isNaN(e.target.textContent || e.target.textContent == '=')) {
-      memory.push(e.target.textContent);
-      // return console.log(e.target.textContent);
-    }
-
-    // Equals logic
-    if (e.target.textContent === '=') {
-      console.log('pressed on equals');
-    }
-  })
-);
-
-const operator = function (opt, ...arr) {
-  let total;
-
-  const [num1, num2] = arr;
-
-  function sum(num1, num2) {
-    return num1 + num2;
-  }
-  function sub(num1, num2) {
-    return num1 - num2;
-  }
-  function mul(num1, num2) {
-    return num1 * num2;
-  }
-  function div(num1, num2) {
-    return num1 / num2;
+class Calculator {
+  constructor(previousNumEl, currentNumEl) {
+    this.previousNumEl = previousNumEl;
+    this.currentNumEl = currentNumEl;
   }
 
-  if (opt === 'add') return sum(num1, num2);
+  clear() {}
 
-  if (opt === 'subtract') return sub(num1, num2);
+  delete() {}
 
-  if (opt === 'multiply') return mul(num1, num2);
+  appendNumber(number) {}
 
-  if (opt === 'divide') return div(num1, num2);
-};
+  chooseOperator(operator) {}
+
+  compute() {}
+
+  updateDisplay() {}
+}
+
+const calculator = new Calculator(previousNumEl, currentNumEl);
