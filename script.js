64b6ce5b0d0d75e25d61f978bd73db9cd8abf331
Ervin <evgnz24@gmail.com>
2022-05-04 17:24:58 -0500

    Calculator logic working, all methods in class work

--- script.js
+++ script.js
@@ -1,39 +1,112 @@
 'use strict';
 
-const clearEl = document.querySelector('.clear');
-const deleteEl = document.querySelector('.delete');
-const equalEl = document.querySelector('.equal');
-const numberElNode = document.querySelectorAll('[data-number]');
+const clearEl = document.querySelector('[data-clear]');
+const deleteEl = document.querySelector('[data-delete]');
+const equalEl = document.querySelector('[data-equal]');
+const numbersEl = document.querySelectorAll('[data-number]');
 const operatorElNode = document.querySelectorAll('[data-operator]');
-const previousNumEl = document.querySelector('.previous-number');
-const currentNumEl = document.querySelector('.current-number');
+const previousNumElText = document.querySelector('[data-previous-number]');
+const currentNumElText = document.querySelector('[data-current-number]');
 
 class Calculator {
-  constructor(previousNumEl, currentNumEl) {
-    this.previousNumEl = previousNumEl;
-    this.currentNumEl = currentNumEl;
+  constructor(previousNumElText, currentNumElText) {
+    this.previousNumElText = previousNumElText;
+    this.currentNumElText = currentNumElText;
     this.clear();
   }
 
   clear() {
     this.previousNumEl = '';
     this.currentNumEl = '';
-    this.operatorElNode = undefined;
+    this.operator = undefined;
   }
 
-  delete() {}
+  delete() {
+    this.currentNumEl = this.currentNumEl.toString().slice(0, -1);
+  }
 
   appendNumber(number) {
-    this.currentNumEl = number;
+    // If there is a decimal already or we pressed a decimal then dont;'t do anything
+
+    if (number === '.' && this.currentNumEl.includes('.')) return;
+    this.currentNumEl = this.currentNumEl.toString() + number.toString();
   }
 
-  chooseOperator(operator) {}
+  chooseOperator(operator) {
+    if (this.currentNumEl === '') return;
+    if (this.previousNumEl !== '') {
+      this.compute();
+    }
+    this.operator = operator;
+    this.previousNumEl = this.currentNumEl;
+    this.currentNumEl = '';
+  }
 
-  compute() {}
+  compute() {
+    let computation;
+    const prev = +this.previousNumEl;
+    const curr = +this.currentNumEl;
+
+    if (isNaN(prev) || isNaN(curr)) return;
+
+    switch (this.operator) {
+      case '+':
+        computation = prev + curr;
+        break;
+      case '-':
+        computation = prev - curr;
+        break;
+      case '*':
+        computation = prev * curr;
+        break;
+      case '÷':
+        computation = prev / curr;
+        break;
+      default:
+        return;
+    }
+    this.currentNumEl = computation;
+    this.operator = undefined;
+    this.previousNumEl = '';
+  }
 
   updateDisplay() {
-    this.currentNumEl.innerText = this.currentNumEl;
+    this.currentNumElText.innerText = this.currentNumEl;
+    if (this.operator != null) {
+      this.previousNumElText.innerText = `${this.previousNumEl} ${this.operator}`;
+    } else {
+      this.previousNumElText.innerText = '';
+    }
   }
 }
 
-const calculator = new Calculator(previousNumEl, currentNumEl);
+const calculator = new Calculator(previousNumElText, currentNumElText);
+
+numbersEl.forEach((button) => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operatorElNode.forEach((button) => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperator(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+equalEl.addEventListener('click', (e) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+clearEl.addEventListener('click', (e) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+deleteEl.addEventListener('click', (e) => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
